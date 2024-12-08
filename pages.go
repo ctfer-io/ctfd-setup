8f23fd14e868da08cf9e04361ef41331944ddeb2
Lucas TESSON <lucastesson@protonmail.com>
2024-12-08 10:21:09 +0100

    fix: cover whole _pages_ lifecycle (+delete)

--- pages.go
+++ pages.go
@@ -2,23 +2,32 @@ package ctfdsetup
 
 import (
 	"context"
+	"slices"
 	"strconv"
 
 	"github.com/ctfer-io/go-ctfd/api"
 )
 
 func additionalPages(ctx context.Context, client *api.Client, pages []Page) error {
+	ctfdPages, err := client.GetPages(&api.GetPagesParams{}, api.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+	cu := []string{}
+
 	for _, page := range pages {
-		ctfdP, err := client.GetPages(&api.GetPagesParams{
-			Route: ptr(page.Route),
-		}, api.WithContext(ctx))
-		if err != nil {
-			return err
+		var ctfdP *api.Page
+		for _, p := range ctfdPages {
+			if p.Route == page.Route {
+				ctfdP = p
+				break
+			}
 		}
 
-		exist := len(ctfdP) == 1
-		if exist {
-			if _, err := client.PatchPage(strconv.Itoa(ctfdP[0].ID), &api.PatchPageParams{
+		cu = append(cu, page.Route)
+		if ctfdP != nil {
+			// UPDATE
+			if _, err := client.PatchPage(strconv.Itoa(ctfdP.ID), &api.PatchPageParams{
 				Title:        page.Title,
 				Route:        page.Route,
 				Format:       page.Format,
@@ -30,6 +39,7 @@ func additionalPages(ctx context.Context, client *api.Client, pages []Page) erro
 				return err
 			}
 		} else {
+			// CREATE
 			if _, err := client.PostPages(&api.PostPagesParams{
 				Title:        page.Title,
 				Route:        page.Route,
@@ -39,6 +49,15 @@ func additionalPages(ctx context.Context, client *api.Client, pages []Page) erro
 				Hidden:       page.Hidden,
 				AuthRequired: page.AuthRequired,
 			}, api.WithContext(ctx)); err != nil {
+				return err
+			}
+		}
+	}
+
+	// DELETE
+	for _, ctfdP := range ctfdPages {
+		if !slices.Contains(cu, ctfdP.Route) {
+			if err := client.DeletePage(strconv.Itoa(ctfdP.ID)); err != nil {
 				return err
 			}
 		}
